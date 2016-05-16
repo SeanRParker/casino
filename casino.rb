23d7d46895a4ff1a7023af9292f7bfe1d9a3efba
Shaws <seanrichardparker@gmail.com>
2016-05-16 11:54:15 -0600

    matching file to lecture status

--- casino.rb
+++ casino.rb
@@ -1,26 +1,17 @@
-# require relative 'mechanics/dice'
-Dir[File.dirname(--FILE--) + '.mechanics/*.rb'].each {|file| require file}
+# require_relative 'mechanics/dice'
+Dir[File.dirname(__FILE__) + '/mechanics/*.rb'].each {|file| require file}
 
 class Casino
-  include mechanics
+    include Mechanics
 
-  def play
-  puts Mechanics::Dice.roll
-  cards = Mechanics::Deck.new.cards
-  cards.each do |card|
-    puts "#{card.rank} of #{card.suit}"
-  end
-end
-
-
-
-
-
-module Mechanics
-  module Dice
-    def self.roll
-      die = []
-      die << rand(1..6) << rand(1..6)
+    def play
+        puts Mechanics::Dice.roll
+        cards = Mechanics::Deck.new.cards.shuffle
+        cards.each do |card|
+            puts "#{card.rank} of #{card.suit}"
+        end
     end
-  end
 end
+
+@game = Casino.new
+@game.play
