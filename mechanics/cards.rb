cda785cf3920cdf88dfecfdbfbe20e6e33d6b54c
Shaws <seanrichardparker@gmail.com>
2016-05-16 11:57:58 -0600

    synchronizing card file to lecture notes

--- mechanics/cards.rb
+++ mechanics/cards.rb
@@ -1,30 +1,30 @@
 module Mechanics
-  class Card
-    attr_accessor :rank, :suit, :color
-    #card = Card.new('10', 'Spade', 'Black')
-    def initialize
-      @rank = rank
-      @suit = suit
-      @color = (suit == 'Spades' || suit == 'Clubs')? 'Black' : 'Red'
-    end
-  end
+   class Card
+       attr_accessor :rank, :suit, :color
+       #card = Card.new('k','Spade', 'Black')
+       def initialize(rank, suit)
+           @rank = rank
+           @suit = suit
+           @color = (suit == 'Spades' || suit == "Clubs")? 'Black' : 'Red'
+       end
+   end
 
-  class Deck
-    attr_accessor :cards
+   class Deck
+       attr_accessor :cards
 
-    def initialize
-      @ranks = %w(A 2 3 4 5 6 7 8 9 10 J Q K)
-      @suits = %w(Spades Diamonds Clubs Hearts)
-      @cards = []
-      generate_deck
-    end
+       def initialize
+           @ranks = %w(A 2 3 4 5 6 7 8 9 10 J Q K)
+           @suits = %w(Spades Diamonds Clubs Hearts)
+           @cards = []
+           generate_deck
+       end
 
-    def generate_deck
-      @suits.each do |suit|
-        @rank.each do |rank|
-          @card << Card.new(rank, suit)
-
-      end
-    end
-  end
+       def generate_deck
+           @suits.each do |suit|
+               @ranks.each do |rank|
+                   @cards << Card.new(rank, suit)
+               end
+           end
+       end
+   end
 end
