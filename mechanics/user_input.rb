10a50555410417b25a36c9292a0dc69626bc3691
Henry Doan <henrydoan96@gmail.com>
2016-05-16 13:21:46 -0600

    Userinput

---
+++ mechanics/user_input.rb
@@ -0,0 +1,27 @@
+module Mechanics
+
+class User_profile
+# @user = {name => total}
+@user =[]
+@money =[]
+
+	puts "What is your name?"
+	name = gets.strip
+	@user << name
+	puts "How much is in your wallet?"
+	total = gets.strip.to_f
+	@money << total
+	puts @user
+	puts @money
+end
+
+end
+
+# class User_profile
+# 	attr_accessor :name, :total
+
+# 	def initialize(name, total)
+# 		@name = name
+# 		@total = total
+# 	end
+# end
